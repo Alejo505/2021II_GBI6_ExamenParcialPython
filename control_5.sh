cc543d43c1f6c7af22095fe4247adaba51548cd9
Alejo505 <ariel.parra@est.ikiam.edu.ec>
2022-02-25 22:05:30 -0500

    Alejandro Parra ha realizado el ejercicio 5

---
+++ control_5.sh
@@ -0,0 +1,7 @@
+#### Interpretación de la distribución normal
+Esto nos da a entender que el o los artículos que más se podrán encontrar son los de los años que se acercan a la media.
+#### Interpretación del boxplot
+El boxplot nos miestra que el 50% de autores se encuentran entre 5 y 15 artículos.
+#### Interpretación de la nube de puntos
+La nube de puntos indica a 3 países en donde el tamaño del punto define la cantidad de autores por cada país, en
+este caso se elijió a Ecuador, Grecia y Filipinas, los cuales no varían mucho en tamaño y por lo tanto tampoco en cantidad.
